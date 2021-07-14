b92546d4e4b31fd0ca7ef882894916a20a24742d
acen20 <ahsen1330@gmail.com>
2021-07-14 18:17:04 +0500

    Completed

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "main"
+
 urlpatterns = [
-    path('', views.index, name = "index")
+    path('', views.index, name = "index"),
+    path('add_to/', views.add_to, name = "add_to"),
+    path('delete_item/<int:item_id>', views.delete_item, name = "delete_item"),
 ]
--- main/views.py
+++ main/views.py
@@ -2,23 +2,36 @@ from django.shortcuts import render, get_object_or_404
 from .models import Todo
 from django.utils import timezone
 from django.template import Context
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
 def index(request):
     context = {
         'Exist': 1,
-        'objs': 0
+        'objs': 0,
+        'year': timezone.now().year
     }
 
     try:
-        objs = Todo.objects.all()
+        objs = Todo.objects.all().order_by('-added_date')
         context['objs'] = objs
 
     except (KeyError, Todo.DoesNotExist):
-        context['Exist'] = False
+        context['Exist'] = 0
 
-    if request.POST:
-        item = request.POST['item']
-        todo = Todo(text = item, added_date = timezone.now())
-        todo.save()
     return render(request, "main/index.html", context)
+
+
+def add_to(request):
+    item = request.POST['item']
+    todo = Todo(text = item, added_date = timezone.now())
+    todo.save()
+    return HttpResponseRedirect(reverse("main:index"))
+
+@csrf_exempt
+def delete_item(request, item_id):
+    Todo.objects.get(id = item_id).delete()
+    return HttpResponseRedirect(reverse("main:index"))
